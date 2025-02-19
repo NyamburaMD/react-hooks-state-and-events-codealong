12f490f506efdfd8b32b5a0a4edba2d6c7317120
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2025-02-19 12:07:08 +0300

    adding the color

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react"; //import state hook
 
 function Toggle() {
-  function handleClick() {
-    console.log("On");
+  const [isOn, setIsOn] = useState(false); //initial state and using state var
+
+  function handleClick() { //handleClick callback
+    setIsOn((isOn) => !isOn); //update using setter function
   }
-  return <button onClick={handleClick}>OFF</button>;
+
+  const color = isOn ? "red" : "white" //conditional rendering for whether the button comes out red or white
+
+  return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>; //Conditional rendering
 }
 
 export default Toggle;
